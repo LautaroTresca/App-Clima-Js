6b45e660f2382b88f577f1ce7b3c270d9eccd1ca
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-05-24 18:50:50 -0300

    agregado de estilos y minimos arreglos en js

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 const apiKey = "371db5a5593ac42270ca5901112ec179";
-const form = document.getElementById("form")
-const main = document.querySelector("main")
+const form = document.getElementById("form");
+let ciudad = document.getElementById("ciudad").value;
+const descripcion = document.getElementById("descripcion");
+const alturaPantalla = window.innerHeight;
+document.body.style.height = alturaPantalla + "px";
 
 function llamarAPI(city,countryCode,apiKey){
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}&units=metric&lang=es`)
     .then((response) => response.json())
     .then((json) => {
-        console.log(json)
-        let ciudad = json.name;
-        let info = json.weather[0];
-        let temperatura = json.main.temp
+        let city = json.name;
+        let info = json.weather[0].description;
+        let temp = json.main.temp
 
-        main.innerHTML += `
-            <p>${ciudad}</p>
-            <p>${info.description}</p>
-            <p>Temperatura: ${temperatura}C°</p>
+        console.log(city);
+        console.log(info);
+        console.log(temp);
+
+        ciudad = `
+            ${city}
         `
-        console.log(ciudad)
-        console.log(info)
-        console.log(temperatura)
+
     })
 }
 
 
-form.addEventListener("submit", (event) =>{
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const city = document.getElementById("city").value;
     const countryCode = document.getElementById("country").value;
