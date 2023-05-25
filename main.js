359e934db156965e4bc02624836f71563e6b07f4
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-05-25 15:57:21 -0300

    agregado de icono del clima y arreglos en el codigo js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const apiKey = "371db5a5593ac42270ca5901112ec179";
 const form = document.getElementById("form");
 const alturaPantalla = window.innerHeight;
+const imagenClima = document.getElementById("imagenClima");
 let ciudad = document.getElementById("ciudad");
 let descripcion = document.getElementById("descripcion");
 let temperatura = document.getElementById("temperatura");
@@ -14,23 +15,35 @@ function llamarAPI(city,countryCode,apiKey){
     .then((json) => {
         info = json.weather[0].description;
         temp = json.main.temp;
-        
-        console.log(city)
     })
 }
 
-function cambiarValor() {
+function cambiarPresentacion() {
     ciudad.value = city.value.toUpperCase();
-    descripcion.value = info.valueOf().toUpperCase();
+    cambiarInfoClima();
     temperatura.value = parseInt(temp.valueOf()) + "°C" ;
 }
 
+function cambiarInfoClima() {
+    if(info.valueOf() === "nubes"){
+        imagenClima.src = "images/nube.png";
+        descripcion.value = "Nuboso";
+    }
+    else if(info.valueOf() === "muy nuboso"){
+        imagenClima.src = "images/nubes.png";
+        descripcion.value = "Muy Nuboso";
+    }
+    else if(info.valueOf() === "lluvia moderada"){
+        imagenClima.src = "images/lluvia.png";
+        descripcion.value = "Lluvia Moderada";
+    }
+}
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     const city = document.getElementById("city").value;
     const countryCode = document.getElementById("country").value;
     llamarAPI(city,countryCode,apiKey);
-    setTimeout(cambiarValor, 1000);
+    setTimeout(cambiarPresentacion, 1500);
 })
 
