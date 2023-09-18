2d1cf369782b713d134f901a70fa5761a8699edd
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-09-18 18:19:13 -0300

    mejoras en los iconos del estado del tiempo y agregado de datos del tiempo actual

--- main.js
+++ main.js
@@ -7,8 +7,14 @@ const main = document.querySelector("main")
 let ciudad = document.getElementById("ciudad");
 let descripcion = document.getElementById("descripcion");
 let temperatura = document.getElementById("temperatura");
+let humedad = document.getElementById("humedad");
+let viento = document.getElementById("viento");
+let sensacion_termica= document.getElementById("sensTermica");
 let info = "";
 let temp = "";
+let hum = "";
+let vient = "";
+let sensTermica = "";
 document.body.style.height = alturaPantalla + "px";
 
 if(ciudad.value == ""){
@@ -22,6 +28,9 @@ function llamarAPI(city,apiKey){
     .then((json) => {
         info = json.weather[0].description;
         temp = json.main.temp;
+        sensTermica = json.main.feels_like;
+        hum = json.main.humidity;
+        vient = json.wind.speed;
     })
 }
 
@@ -29,14 +38,24 @@ function cambiarPresentacion() {
     ciudad.value = city.value.toUpperCase();
     cambiarInfoClima();
     temperatura.value = parseInt(temp.valueOf()) + "°C" ;
+    humedad.value = parseInt(hum.valueOf()) + "%";
+    viento.value = parseInt(vient.valueOf()) + "km/h";
+    sensacion_termica.value = parseInt(sensTermica.valueOf()) + "°C";
 }
-
 function cambiarInfoClima() {
     main.style.animation = "fadeIn 1s";
     seccionInfo.style.display = "flex";
     if(info.valueOf() === "nubes"){
         imagenClima.src = "images/nube.png";
         descripcion.value = "Nuboso";
+    }
+    if(info.valueOf() === "cielo claro"){
+        imagenClima.src = "images/sol.png";
+        descripcion.value = "Soleado";
+    }
+    if(info.valueOf() === "nubes dispersas"){
+        imagenClima.src = "images/nubes_dispersas.png";
+        descripcion.value = "Nubes Dispersas";
     }
     else if(info.valueOf() === "muy nuboso"){
         imagenClima.src = "images/nubes.png";
@@ -47,6 +66,8 @@ function cambiarInfoClima() {
         descripcion.value = "Lluvia Moderada";
     }
 }
+
+
 
 form.addEventListener("submit", (event) => {
     main.style.animation = "fadeOut 1.7s";
